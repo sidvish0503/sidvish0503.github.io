e5a5237b5f7c1df58b458806508df2daa35f79c4
sidvish0503 <wildbobcat20@gmail.com>
2026-06-25 15:48:54 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,8 @@
 console.log("This is loaded from the external JS file");
+var name = "Sid";
+let school = "Lake Norman Charter High School";
+var fun_fact = "I love listening to music and I want to become a producer";
+
+console.log(`Name: $(name)`); 
+console.log(`School: ${school}`);
+console.log(`Fun Fact: ${fun_fact}`);
